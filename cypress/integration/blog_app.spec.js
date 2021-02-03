9f200bd91d4b7b5a273b0c75b95d68e341b63a6a
Pauli Partanen <pauli.tapio.partanen@outlook.com>
2021-02-03 11:36:15 +0200

    5.22 done

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -42,10 +42,11 @@ describe('Blog app', function () {
 
   describe('when logged in', function () {
     beforeEach(function () {
-      cy.get('#username').type('pablo')
-      cy.get('#password').type('salainen')
-      cy.get('#login-button').click()
 
+      cy.login({ username: 'pablo', password: 'salainen' })
+    })
+
+    it('a new blog can be created', function () {
       cy.contains('new note').click()
 
       cy.get('#title').type('React tips and tricks')
@@ -53,15 +54,6 @@ describe('Blog app', function () {
       cy.get('#url').type('www.react.com')
       cy.get('#createblog').click()
 
-      cy.contains('new note').click()
-
-      cy.get('#title').type('Javascript is fun')
-      cy.get('#author').type('Javascript Guru')
-      cy.get('#url').type('www.js.com')
-      cy.get('#createblog').click()
-    })
-
-    it('a new blog can be created', function () {
       cy.contains('a new blog React tips and tricks by React Guru added')
       cy.contains('React tips and tricks')
       cy.contains('React Guru')
@@ -70,6 +62,14 @@ describe('Blog app', function () {
     })
 
     it('a blog can be liked', function () {
+
+      cy.createBlog({
+        title: 'title 0',
+        author: 'author 0',
+        url: 'www.url0.com',
+        likes: 0
+      })
+
       cy.contains('view').click()
       cy.contains('likes 0')
       cy.contains('like').click()
@@ -77,12 +77,58 @@ describe('Blog app', function () {
       cy.contains('like').click()
       cy.contains('likes 2')
     })
-    
-    it.only('a blog can be deleted by the person who created it', function () {
+
+    it('a blog can be deleted by the person who created it', function () {
+
+      cy.createBlog({
+        title: 'title 0',
+        author: 'author 0',
+        url: 'www.url0.com',
+        likes: 0
+      })
+
       cy.contains('view').click()
       cy.contains('remove').click()
       cy.get('#blogShortView').should('not.contain', 'React tips and tricks')
       cy.get('#blogShortView').should('not.contain', 'React Guru')
     })
+
+    it('blogs are sorted by the amount of likes in descending order', function () {
+      cy.createBlog({
+        title: 'title 1',
+        author: 'author 1',
+        url: 'www.url1.com',
+        likes: 1
+      })
+      cy.createBlog({
+        title: 'title 2',
+        author: 'author 2',
+        url: 'www.url2.com',
+        likes: 2
+      })
+      cy.createBlog({
+        title: 'title 3',
+        author: 'author 3',
+        url: 'www.url3.com',
+        likes: 3
+      })
+      cy.createBlog({
+        title: 'title 4',
+        author: 'author 4',
+        url: 'www.url4.com',
+        likes: 4
+      })
+      cy.get('.blogWhole').then(blogs => {
+        /* console.log('---------------------------')
+        console.log(blogs[0].innerText)
+        console.log('---------------------------') */
+        expect(blogs[0].innerText).contains('likes 4')
+        expect(blogs[1].innerText).contains('likes 3')
+        expect(blogs[2].innerText).contains('likes 2')
+        expect(blogs[3].innerText).contains('likes 1')
+
+      })
+    })
   })
+
 })
